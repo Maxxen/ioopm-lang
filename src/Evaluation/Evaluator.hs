5463f817ab14ce1e17c5e90a7d71b7d75e301730
Maxxen <max@gabrielsson.com>
2019-01-21 00:10:56 +0100

    greatly simplified evaluator...

--- src/Evaluation/Evaluator.hs
+++ src/Evaluation/Evaluator.hs
@@ -4,78 +4,71 @@ import Control.Monad.State
 import Control.Monad.Writer
 import Control.Monad.Except
 import Control.Monad.Identity
-import Control.Monad.Trans.Either
 import qualified Data.HashMap.Lazy as H
-
 
 type Var = String
 type Environment = H.HashMap Var Expr
 type EvalError = String
 
-type Evaluator = EitherT Expr (StateT Environment (ExceptT EvalError Identity))
+type Evaluator = StateT Environment (ExceptT EvalError Identity)
 
 runEvaluator :: Evaluator a -> Environment -> Either EvalError (a, Environment)
-runEvaluator ev env = case runIdentity $ runExceptT $ runStateT (runEitherT ev) env of
+runEvaluator ev env = case runIdentity $ runExceptT $ runStateT ev env of
   (Left err) -> Left err
-  (Right c) -> case c of
-                 (Left expr, s) -> Left "EXPRESSION NOT FULLY EVALUATED"
-                 ((Right val), s) -> Right (val, s)
-
+  (Right (r, s)) -> Right(r, s)
   
-type Evaluation = Evaluator Float
+type Evaluation = Evaluator Expr
 
 -- Strict evaluation
 
 evaluate :: Expr -> Evaluation
-evaluate (Constant x) = do
-  return x
+evaluate (Constant x) = return (Constant x)
 
 evaluate (Binary Assignment lhs (Identifier str)) = do
   val <- evaluate lhs
-  modify $ (\env -> H.insert str (Constant val) env)
+  modify $ (\env -> H.insert str lhs env)
   return val
 
 evaluate (Binary op lhs rhs) = do
   l <- evaluate lhs
   r <- evaluate rhs
-  case op of
-    Addition -> return $ l + r
-    Subtraction -> return $ l - r
-    Multiplication -> return $ l * r
-    Division -> if r == 0
-      then lift $ lift $ throwError "Division by 0!"
-      else return $ l / r
+  case (l, r) of
+    (Constant lc, Constant rc) -> reduce op lc rc  
+    (le, re) -> return $ Binary op le re
     
+  where
+    reduce :: BOp -> Float -> Float -> Evaluation
+    reduce (Addition) l r = return $ Constant $ l + r
+    reduce (Subtraction) l r = return $ Constant $ l - r
+    reduce (Multiplication) l r = return $ Constant $ l * r
+    reduce (Division) l 0 = lift $ throwError "Division by 0!"
+    reduce (Division) l r = return $ Constant $ l / r
+              
+
 evaluate (Unary op a) = do
   arg <- evaluate a
-  case op of
-    Negation -> return (- arg)
-    Exp -> return (exp arg)
-    Log -> if arg < 0
-      then lift $ lift $ throwError "Logarithm is undefined for x < 0"
-      else return (log arg)
-    Sin -> return (sin arg)
-    Cos -> return (cos arg)
+  case arg of
+    (Constant ac) -> reduce op ac
+    (ae) -> return $ Unary op ae
 
+  where
+    reduce :: UOp -> Float -> Evaluation
+    reduce (Negation) arg = return $ Constant (- arg)
+    reduce (Sin) arg = return $ Constant (sin arg)
+    reduce (Cos) arg = return $ Constant (cos arg)
+    reduce (Exp) arg = return $ Constant (exp arg)
+    reduce (Log) arg = if arg < 0
+                         then lift $ throwError "Logarithm is undefined for x < 0"
+                         else return $ Constant (log arg)
+   
 evaluate (Scope expr) = do
   env <- get -- Copy environment state
   val <- evaluate expr
   put env -- Reset environment state
   return val
 
-evaluate (Identifier str) = do
+evaluate ident@(Identifier str) = do
   env <- get
   case H.lookup str env of
     Just expr -> evaluate expr
-    Nothing -> throwError (Identifier str)
-    --Slight abuse of throwError, but how else am i supposed to return Left?
-
-evaluate (Conditional cond true false) = do
-  c <- evaluate cond
-  if c > 0
-    then do
-      evaluate true
-    else do
-      evaluate false
-
-
+    Nothing -> return ident
